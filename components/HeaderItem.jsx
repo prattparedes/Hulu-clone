9d05a2e51323380fb57bdb429b2202402d8a7499
Pratt <pratt.paredes@gmail.com>
2022-10-19 23:54:42 -0500

    hover effect with tailwind

--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function HeaderItem({ Icon, title }) {
   return (
     <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
-      <Icon className="h-8 mb-1" />
-      <p className="opacity-100 group-hover:opacity-100 tracking-widest font-medium" >{title}</p>
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest font-medium" >{title}</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ module.exports = {
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {
+      animation: ['group-hover'],
+    },
+  },
   plugins: [],
 };
